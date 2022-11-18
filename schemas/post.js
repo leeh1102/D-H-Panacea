1b8f599694ab1aff1bba95b3191a82e8f8a78d06
Heather Lee <leeh1102@gmail.com>
2022-11-18 03:15:28 -0800

    Create post schema

--- schemas/post.js
+++ schemas/post.js
@@ -1,3 +1,18 @@
 
 const mongoose = require('mongoose');
-const { Schema } = require('./schema');
+const { Schema } = mongoose;
+const postSchema = new Schema({
+    title: String,
+    contactNumber: String,
+    description: String,
+    contactEmail: String,
+    serviceLocation: String,
+    serviceType: String, // This is a string for now, but it should be an array of strings
+    hours: String,
+    userType: String,
+    school: String, // only if userType == studentPhysician
+    postDate: String, // This is a string for now, but it should be a date
+})
+
+module.exports.postSchema = postSchema;
+module.exports.Post = mongoose.model('Post', postSchema);
