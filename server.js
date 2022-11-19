8c3f1ce3fead3c71603ccb250f06e5fefa1787a6
msubin <joy120594@gmail.com>
2022-11-19 00:41:41 -0800

    Complete registration post method

--- server.js
+++ server.js
@@ -30,6 +30,50 @@ app.get('/api', (req, res) => {
 })
 
 
+// REGISTER A USER
+const bcrypt = require("bcrypt");
+const User = require("./schemas/user");
+// register endpoint
+app.post("/sign-up", (request, response) => {
+    // hash the password
+    bcrypt
+        .hash(request.body.password, 10)
+        .then((hashedPassword) => {
+            // create a new user instance and collect the data
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: request.body.email,
+                password: hashedPassword,
+            });
+
+            // save the new user
+            user
+                .save()
+                // return success if the new user is added to the database successfully
+                .then((result) => {
+                    response.status(201).send({
+                        message: "User Created Successfully",
+                        result,
+                    });
+                })
+                // catch error if the new user wasn't added successfully to the database
+                .catch((error) => {
+                    response.status(500).send({
+                        message: "Error creating user",
+                        error,
+                    });
+                });
+        })
+        // catch error if the password hash isn't successful
+        .catch((e) => {
+            response.status(500).send({
+                message: "Password was not hashed successfully",
+                e,
+            });
+        });
+});
+
+
 // Otherise, go to the FE
 app.listen(port, (err) => {
     if (err) return console.loge(err);
